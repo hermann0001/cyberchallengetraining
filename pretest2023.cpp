2874d049f3bf4333dfc6b3fa5ef064ddf373769e
Utente <utente@lding-c46.studenti.local>
2024-01-17 19:02:12 +0100

    first commit

---
+++ pretest2023.cpp
@@ -0,0 +1,13 @@
+/*
+    Input are N+2 lines:
+    l1. Q - number of questions, N - candidates
+    l2. list of correct answers as a string of Q uppercase letters in the set {A, B, C, D}
+    l3, ..., lN+2. string of Q characters in {A, B, C, D, ?} (? is missing answer)
+*/
+
+#include <iostream>
+
+void readInput();
+void writeOutput();
+void check();
+
