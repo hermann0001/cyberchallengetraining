4899c2c86f16faac55af84f1ae9ba56d477a5166
Utente <utente@lding-c18.studenti.local>
2024-02-05 18:54:56 +0100

    still cant solve input-3

--- nationalfinal/workers.py
+++ nationalfinal/workers.py
@@ -43,7 +43,7 @@ def calculate(N, T, times, workers):
             # heapq.heappush(task_heap, (times[i], workers))
             #return calculate(N, T, times, workers+1)
             workers += 1
-            #task_heap = [(0, j) for j in range(workers)]  # Re-initialize the heap
+            task_heap = [(0, j) for j in range(workers)]  # Re-initialize the heap
             i = 0
             print("workers:", workers)
 
@@ -53,7 +53,39 @@ def calculate(N, T, times, workers):
             heapq.heappush(task_heap, (finish_time, worker_index))
             i += 1
 
-    return workers         
+    return workers
+        
+# def can_complete_tasks(workers, N, T, times):
+    
+#     #Checks if a given number of workers can complete all tasks within the time limit T.
+#     print(workers)
+#     # Initialize a priority queue with workers' next available time starting at 0
+#     task_heap = [(0, i) for i in range(workers)]
+#     for time in times:
+#         finish_time, worker_index = heapq.heappop(task_heap)
+#         if finish_time + time > T:
+#             # If adding the current task exceeds T, return False
+#             return False
+#         heapq.heappush(task_heap, (finish_time + time, worker_index))
+#     # If all tasks can be assigned without exceeding T, return True
+#     return True   
+
+# def calculate(N, T, times):
+
+#     #Uses binary search to find the minimum number of workers needed to complete all tasks within T.
+    
+#     # Sort times to potentially optimize the distribution of tasks
+#     times.sort(reverse=True)
+#     left, right = 1, N  # At least one worker is needed, and at most one worker per task
+    
+#     while left < right:
+#         mid = (left + right) // 2
+#         if can_complete_tasks(mid, N, T, times):
+#             right = mid
+#         else:
+#             left = mid + 1
+    
+#     return left      
 
 if __name__ == "__main__":
 
@@ -62,7 +94,7 @@ if __name__ == "__main__":
         sys.exit(1)
     input_file = os.path.join(INPUT_PATH, sys.argv[1])
     N, T, tasks_time = read_input(input_file)
-    W = calculate(N, T, tasks_time, math.ceil(sum(tasks_time) / T))
+    W = calculate(N, T, tasks_time, 101088)#math.ceil(sum(tasks_time) / T))
 
     output_file = os.path.join(OUTPUT_PATH, "output.txt")
     write_output(output_file, W)
